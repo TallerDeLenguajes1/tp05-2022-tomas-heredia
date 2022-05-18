3c3ce8f8f6689897ecf3144eb3efa48dfb76f15e
tomas-heredia <tomasheredia04@hotmail.com>
2022-05-18 14:48:05 -0300

    punto 1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,4 +1,22 @@
-﻿Console.WriteLine("ingrese texto");
+﻿Console.WriteLine("ingrese el numero");
 string texto = Console.ReadLine();
-int numero = 4;
-Console.WriteLine("escribio el numero: "+ texto);
+int numero = Int32.Parse(texto);
+int invertido = 0;
+int aux = 0;
+if (numero > 0)
+{
+    while (aux == 0)
+    {
+        if (numero >= 10)
+        {
+            invertido = invertido*10 + numero%10;
+            numero = numero/10;
+        }else
+        {
+            invertido = invertido*10 + numero;
+            aux = 1;
+        } 
+    }
+    
+}
+Console.Write("Numero invertido = "+invertido);
